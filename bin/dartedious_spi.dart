58c14fe439e499e445e5306485166e819f348fba
pheonixeye <drkazgm@gmail.com>
2023-11-06 22:57:35 +0200

    empty project

---
+++ bin/dartedious_spi.dart
@@ -0,0 +1,5 @@
+import 'package:dartedious_spi/dartedious_spi.dart' as dartedious_spi;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dartedious_spi.calculate()}!');
+}
---
+++ test/dartedious_spi_test.dart
@@ -0,0 +1,8 @@
+import 'package:dartedious_spi/dartedious_spi.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
